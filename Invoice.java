1a934cd87aaf7bcee8775a90d4b435c5798b7f7b
Aurelia-Lois <ama5724@students.uc-bcf.edu.ph>
2024-12-02 14:49:17 +0800

    Add files via upload

---
+++ Invoice.java
@@ -0,0 +1,22 @@
+public class Invoice {
+	public static void main (String[] args){
+		//Store Information
+		System.out.print("\nEloisa\'s School and Office Supplies Store\t\t\t\t\t\t08/09/2024");
+		System.out.print("\n447, Purok 4, Balsigan\n\nBilled To:Angwani,Aurelia Lois\n");
+		
+		
+		//Item List
+		System.out.print("\n_______________________________________________________________________\n");	
+		System.out.print("\nItem\t\t\tQuantity\tUnit Price\tTotal");
+		System.out.print("\n_______________________________________________________________________\n");
+		System.out.print("\nYellow\tPad\t\t2\t\tP55\t\tP110\n");
+		System.out.print("\nCalculator\t\t1\t\tP200\t\tP200\n");
+		System.out.print("\nBond Paper\t\t1\t\tP200\t\tP200\n");
+		System.out.print("\nFolder file Organizer\t3\t\tP90\t\tP270\n");
+		System.out.print("\n_______________________________________________________________________\n");
+		System.out.print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tTotal");
+		System.out.println("\t\tP780");
+		//Thank You Customers
+		System.out.print("\nThank You\n");
+	}
+}
